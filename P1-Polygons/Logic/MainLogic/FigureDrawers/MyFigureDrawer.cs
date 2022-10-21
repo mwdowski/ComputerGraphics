04c2967b0f4aba1452ae4019efecae70aa4def6f
Michał Wdowski <michal.wdowski99@gmail.com>
2022-10-21 23:52:06 +0200

    Implement Bresenham algorithm

--- P1-Polygons/Logic/MainLogic/FigureDrawers/MyFigureDrawer.cs
+++ P1-Polygons/Logic/MainLogic/FigureDrawers/MyFigureDrawer.cs
@@ -1,9 +1,11 @@
 ﻿using P1_Polygons.Model;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace P1_Polygons.Logic.MainLogic.FigureDrawers
 {
@@ -15,30 +17,43 @@ namespace P1_Polygons.Logic.MainLogic.FigureDrawers
 
         public override void DrawEdge(Edge edge, Graphics graphics)
         {
-            var rasterizedStart = Rasterizer.Rasterize(edge.Start.Position); 
-            var rasterizedEnd = Rasterizer.Rasterize(edge.End.Position); 
-            int dx = rasterizedEnd.X - rasterizedStart.X;
-            int dy = rasterizedEnd.Y - rasterizedStart.Y;
-            int d = 2 * dy - dx;
-            int incrE = 2 * dy;
-            int incrNE = 2 * (dy - dx);
+            var rasterizedStart = Rasterizer.Rasterize(edge.Start.Position);
+            var rasterizedEnd = Rasterizer.Rasterize(edge.End.Position);
             int x = rasterizedStart.X;
             int y = rasterizedStart.Y;
+            int dx = rasterizedEnd.X - rasterizedStart.X;
+            int dy = rasterizedEnd.Y - rasterizedStart.Y;
 
-            while (x < rasterizedEnd.X)
+            int dx1 = 0, dy1 = 0, dx2 = 0, dy2 = 0;
+            if (dx < 0) dx1 = -1; else if (dx > 0) dx1 = 1;
+            if (dy < 0) dy1 = -1; else if (dy > 0) dy1 = 1;
+            if (dx < 0) dx2 = -1; else if (dx > 0) dx2 = 1;
+
+            int longest = Math.Abs(dx);
+            int shortest = Math.Abs(dy);
+            if (!(longest > shortest))
             {
-                if (d < 0) //choose E
-                {
-                    d += incrE;
-                    x++;
-                }
-                else //choose NE
-                {
-                    d += incrNE;
-                    x++;
-                    y++;
-                }
+                longest = Math.Abs(dy);
+                shortest = Math.Abs(dx);
+                if (dy < 0) dy2 = -1; else if (dy > 0) dy2 = 1;
+                dx2 = 0;
+            }
+            int d = longest >> 1;
+            for (int i = 0; i <= longest; i++)
+            {
                 graphics.FillRectangle(Pen.Brush, x, y, 1, 1);
+                d += shortest;
+                if (!(d < longest))
+                {
+                    d -= longest;
+                    x += dx1;
+                    y += dy1;
+                }
+                else
+                {
+                    x += dx2;
+                    y += dy2;
+                }
             }
         }
     }
